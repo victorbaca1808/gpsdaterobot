0455d0d15e05d04a9884466bb53f0f74b8d0f954
vbaca <vbaca@canvia.com>
2023-09-11 01:47:02 -0500

    generando back gps combi

--- src/main/java/com/peru/combi/repository/PruebaGCbRepository.java
+++ src/main/java/com/peru/combi/repository/PruebaGCbRepository.java
@@ -1,9 +1,9 @@
 package com.peru.combi.repository;
 
-import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.peru.combi.clases.PruebaGCb;
 

7498af0d4e00ddad8ac76d71830969ab9e73e9a8
vbaca <vbaca@canvia.com>
2023-09-11 01:54:32 -0500

    generando back gps combi

--- src/main/java/com/peru/combi/repository/PruebaGCbRepository.java
+++ src/main/java/com/peru/combi/repository/PruebaGCbRepository.java
@@ -1,7 +1,7 @@
 package com.peru.combi.repository;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +9,6 @@ import com.peru.combi.clases.PruebaGCb;
 
 @Repository
 @Transactional
-public interface PruebaGCbRepository  extends CrudRepository<PruebaGCb, Long> {
+public interface PruebaGCbRepository  extends JpaRepository<PruebaGCb, Long> {
     
 }

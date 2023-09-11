808e6e9acb7427e196dc34352a2401f2897fd08b
vbaca <vbaca@canvia.com>
2023-09-11 01:34:38 -0500

    generando back gps combi

--- src/main/java/com/peru/combi/CombiApplication.java
+++ src/main/java/com/peru/combi/CombiApplication.java
@@ -2,8 +2,10 @@ package com.peru.combi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.peru.combi.repository")
 public class CombiApplication {
 
 	public static void main(String[] args) {

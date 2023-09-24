f5a8cbd7ec2e0ba7f2f110af36c0bebc03a410c6
vbaca <vbaca@canvia.com>
2023-09-24 15:20:30 -0500

    generando back gps combi

--- src/main/java/com/peru/combi/repository/PruebaGCbRepository.java
+++ src/main/java/com/peru/combi/repository/PruebaGCbRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.peru.combi.clases.PruebaGCb;
@@ -16,7 +17,9 @@ import com.peru.combi.clases.lastLocations;
 @Transactional
 public interface PruebaGCbRepository  extends JpaRepository<PruebaGCb, Long> {
     
-	@Query( value ="Select gps_Coordenadas as ubication, '' as Direccion from pruebagcb where numero_Telefono = :numTelefono order by fecha_Registro desc limit 3", nativeQuery = true)
+	@Transactional(isolation = Isolation.REPEATABLE_READ)
+	@Query( value ="Select gps_Coordenadas as ubication, '' as Direccion from pruebagcb where numero_Telefono = :numTelefono " +
+	"order by fecha_Registro desc limit 3;",nativeQuery = true)
 	List<lastLocations> obtenerUltimas3Ubicaciones(@Param("numTelefono") String numTelefono);
 
 }
